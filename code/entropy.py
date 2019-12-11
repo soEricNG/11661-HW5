84423768c0fda218ca0729ac0460a6893bedc803
kunalkhadilkar <kunalkhadilkar@gmail.com>
2019-12-11 01:57:54 -0500

    Entropy of characters and words

--- code/entropy.py
+++ code/entropy.py
@@ -6,10 +6,11 @@ def Entropy(inputfile,base = 2.0):
 	text = ""
 	with open(inputfile, "r",encoding="utf8") as fd:
 		for line in fd:
+			line = line.replace("\n","")
 			text += line
 
 	dct = dict.fromkeys(list(text))
-
+	#print(dct)
 	#calculate frequencies
 	pkvec =  [float(text.count(c)) / len(text) for c in dct]
 
@@ -17,9 +18,33 @@ def Entropy(inputfile,base = 2.0):
 	H = -sum([pk  * math.log(pk) / math.log(base) for pk in pkvec ])
 	return H
 
+def Entropyofwords(inputfile,base = 2.0):
+	total = 0
+	numofwords = 0
+	text = ""
+	with open(inputfile, "r",encoding="utf8") as fd:
+		for line in fd:
+			line = line.replace("\n","")
+			text += line
+
+	#print(text)
+	dict1 = {}
+	#print(text)
+	for i in text.split():
+		if i not in dict1:
+			dict1[i] = 1
+		else:
+			dict1[i] += 1
+
+	total = sum(dict1.values())
+	pkvec = [float(dict1[c]) / total for c in dict1]
+
+	H = -sum([pk  * math.log(pk) / math.log(base) for pk in pkvec ])
+	return H
+
 def main():
-	#print(Entropy("F:/CMU Courses/11661 Language and Statistics/Assignment 2/warandpeacedataset.txt"))
 	"""
+	#print(Entropy("F:/CMU Courses/11661 Language and Statistics/Assignment 2/warandpeacedataset.txt"))
 	print(Entropy("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/1.txt"))
 	print(Entropy("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/2.txt"))
 	print(Entropy("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/3.txt"))
@@ -33,10 +58,25 @@ def main():
 	print(Entropy("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/11.txt"))
 	print(Entropy("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/12.txt"))
 	print(Entropy("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/13.txt"))
+	#print(Entropy(sys.argv[1]))
 	"""
 	print(Entropy(sys.argv[1]))
-
-
+	print(Entropyofwords(sys.argv[1]))
+	"""
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/1.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/2.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/3.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/4.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/5.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/6.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/7.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/8.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/9.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/10.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/11.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/12.txt"))
+	print(Entropyofwords("F:/CMU Courses/11661 Language and Statistics/Assignment 5/data/13.txt"))
+	"""
 if __name__ == "__main__":
 	main()
 
